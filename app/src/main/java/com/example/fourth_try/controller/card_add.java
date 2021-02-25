f3c6869a4f19d0b410171bec8df11c23cda356a2
FeArZ0z <max.spiesmaier@aon.at>
2021-02-25 16:15:31 +0100

    added fragments in card_add.java switch to change fragments implemented

--- app/src/main/java/com/example/fourth_try/controller/card_add.java
+++ app/src/main/java/com/example/fourth_try/controller/card_add.java
@@ -8,8 +8,17 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.fourth_try.R;
+import com.example.fourth_try.view.fragment_blank;
+import com.example.fourth_try.view.fragment_caliente;
+import com.example.fourth_try.view.fragment_challenge;
+import com.example.fourth_try.view.fragment_normal;
+import com.example.fourth_try.view.fragment_question;
+import com.example.fourth_try.view.fragment_vote;
 
 public class card_add extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
@@ -27,33 +36,97 @@ public class card_add extends AppCompatActivity implements AdapterView.OnItemSel
         dropdown.setAdapter(new ArrayAdapter<KindOfCard>(this, android.R.layout.simple_spinner_dropdown_item, KindOfCard.values()));
         dropdown.setOnItemSelectedListener(this);
 
+
+
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View v, int i, long id) {
 
-        //TODO Change up the int i with ENUM
+        changeFragment(i);
+
+        /**
+         * Case 0: Nothing
+         * Case 1: Normal
+         * Case 2: Caliente
+         * Case 3: Question
+         * Case 4: Challenge
+         * Case 5: Vote
+         */
+/**
         switch (i) {
             case 0:
                 Toast.makeText(getApplicationContext(), "Select something", Toast.LENGTH_SHORT).show();
+                changeFragment(i);
                 break;
-            case 10:
+            case 1:
                 Toast.makeText(getApplicationContext(), "Normal", Toast.LENGTH_SHORT).show();
+                changeFragment(i);
+                break;
             case 2:
                 Toast.makeText(getApplicationContext(), "Caliente", Toast.LENGTH_SHORT).show();
+                changeFragment(i);
+                break;
             case 3:
                 Toast.makeText(getApplicationContext(),"Question", Toast.LENGTH_SHORT).show();
+                changeFragment(i);
+                break;
             case 4:
                 Toast.makeText(getApplicationContext(),"Challenge", Toast.LENGTH_SHORT).show();
+                break;
             case 5:
                 Toast.makeText(getApplicationContext(), "Vote", Toast.LENGTH_SHORT).show();
-        }
+                break;
+        }*/
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
-        // TODO Auto-generated method stub
+        // Auto-generated method stub
     }
 
+    public void changeFragment(int position){
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+
+        /**
+         * Case 0: Nothing
+         * Case 1: Normal
+         * Case 2: Caliente
+         * Case 3: Question
+         * Case 4: Challenge
+         * Case 5: Vote
+         */
+        switch(position){
+            case 0:
+                Toast.makeText(getApplicationContext(), "Select something", Toast.LENGTH_SHORT).show();
+                fragment_blank blankfrag =new fragment_blank();
+                ft.replace(R.id.fragment_questions, blankfrag).commit();
+                break;
+            case 1:
+                fragment_normal normalfrag = new fragment_normal();
+                ft.replace(R.id.fragment_questions, normalfrag).commit();
+                break;
+            case 2:
+                fragment_caliente calfrag = new fragment_caliente();
+                ft.replace(R.id.fragment_questions, calfrag).commit();
+                break;
+            case 3:
+                fragment_question quesfrag = new fragment_question();
+                ft.replace(R.id.fragment_questions, quesfrag).commit();
+                break;
+            case 4:
+                fragment_challenge chalfrag = new fragment_challenge();
+                ft.replace(R.id.fragment_questions, chalfrag).commit();
+                break;
+            case 5:
+                fragment_vote votefrag = new fragment_vote();
+                ft.replace(R.id.fragment_questions, votefrag).commit();
+                break;
+
+        }
+    }
 
 }
